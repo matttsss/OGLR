d893699cd348b07e619b6218298a565b168d3660
Mattéo <mattsantini2003@gmail.com>
2023-09-10 23:30:50 +0200

    Added gitmodules

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,14 @@
 //
 // Created by matts on 10/09/2023.
 //
+
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+
+    glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
+}
